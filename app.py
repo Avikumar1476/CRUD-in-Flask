2d5d62b941cfb049e7e582140e5a91b80c685ab8
avikumar1476 <avinash.kumar@leasingmonk.com>
2022-04-08 13:36:19 +0530

    ...

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
- 
+  
 
 #Creating model table for our CRUD database
 class Data(db.Model):
